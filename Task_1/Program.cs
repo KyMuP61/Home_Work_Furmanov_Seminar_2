1f3d1fa7ecef66b9fa40e68c501f813c6efdcbd7
Alexander Furmanov <kymapcah61@gmail.com>
2022-07-01 23:23:40 +0300

    Finish task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Напишите программу, которая принимает на вход трёхзначное 
+    число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+*/
+
+Console.WriteLine("Enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 1000 && number > 99)
+    Console.WriteLine((number/10)%10);
+else
+    Console.WriteLine("Нужно было ввести трехзначное число");
+
